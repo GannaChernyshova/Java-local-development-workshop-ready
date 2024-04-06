21657a3dd47c2149919182f05275e02e99ec0385
Anna Chernyshova <anna@atomicjar.com>
2024-04-05 20:20:31 -0400

    updated test formatting

--- src/test/java/com/testcontainers/catalog/ContainersConfig.java
+++ src/test/java/com/testcontainers/catalog/ContainersConfig.java
@@ -2,25 +2,18 @@ package com.testcontainers.catalog;
 
 import static org.testcontainers.utility.DockerImageName.parse;
 
-import com.testcontainers.catalog.domain.FileStorageService;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.DependsOn;
-import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.containers.localstack.LocalStackContainer;
-import org.wiremock.integrations.testcontainers.WireMockContainer;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class ContainersConfig {
     @Bean
     @ServiceConnection
     PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>(parse("postgres:16-alpine"))
-                .withReuse(false);
+        return new PostgreSQLContainer<>(parse("postgres:16-alpine")).withReuse(false);
     }
 
     @Bean
@@ -28,5 +21,4 @@ public class ContainersConfig {
     KafkaContainer kafkaContainer() {
         return new KafkaContainer(parse("confluentinc/cp-kafka:7.5.3"));
     }
-
 }
